49235ad8cb8b2da84dd50de124d6dfc9a8b4008f
gawrelyuk <gawrelyuk@ukr.net>
2025-01-13 17:53:41 +0200

    Minor fix

--- src/helpers/utils/getRandomNumber.ts
+++ src/helpers/utils/getRandomNumber.ts
@@ -1,3 +1,3 @@
 export const getRandomNumber = (min: number, max: number): number => {
-    return Math.floor(Math.random() * 10) + 1;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 };
